33fc4f8a23328af0afaa0fd416c6a8eab8f22ad0
Marian <hellomariandev@gmail.com>
2022-07-04 08:45:36 +0200

    created Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import {useState} from "react"
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Star Wars</h1>
+      <Navbar />
+      <div className="content">
+
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <button type="">Planets</button>
+            <button type="">People</button>
+        </nav>
+     );
+}
+ 
+export default Navbar;
