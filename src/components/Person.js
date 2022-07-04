fed0496f2f6cfbc8565cc17207ed7a2aa3d2d54a
Marian <hellomariandev@gmail.com>
2022-07-04 15:14:47 +0200

    updates

--- src/components/Person.js
+++ src/components/Person.js
@@ -2,7 +2,7 @@ const People = ({ people }) => {
   return (
     <div className="card">
       <h3>{people.name}</h3>
-      <p>Population: {people.gender}</p>
+      <p>Gender: {people.gender}</p>
       <p>Birth Year: {people.birth_year}</p>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
