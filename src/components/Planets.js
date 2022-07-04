deaa751551860cc6a113354229f17d9491083471
Marian <hellomariandev@gmail.com>
2022-07-04 13:20:10 +0200

    fetching API with useQuery

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,9 +1,30 @@
+import React from "react";
+import { useQuery } from "react-query";
+
+// async function
+const fetchPlanets = async () => {
+  const res = await fetch("http://swapi.dev/api/planets/");
+  return res.json();
+};
+
 const Planets = () => {
-    return ( 
-        <div>
-            Planets
-        </div>
-     );
-}
- 
+  const { status, error, data } = useQuery("planets", fetchPlanets);
+  console.log(data);
+
+  if (status === "loading") {
+    return <span>Loading...</span>;
+  }
+
+  if (status === "error") {
+    return <span>Something went wrong...</span>;
+  }
+
+  if (status === "success") {
+    return( <div>
+        {data.results.map(planet => <div>{planet.name}</div>)}
+    </div>)
+  }
+
+};
+
 export default Planets;
